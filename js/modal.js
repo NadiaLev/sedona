53fef85ba514ed69f2d6f54e4fefdc728121e6eb
NadiaLev <palash.n@mail.ru>
2020-10-28 11:58:53 +0300

    Внесены изменения по заданию 10

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,18 @@
-"use strict"
+"use strict";
 const search = document.querySelector(".search");
 const button = search.querySelector(".button-search");
 const modal = search.querySelector(".modal");
-const form = modal.querySelector("form");
-const arrival = form.querySelector("[name=arrival]");
+const arrival = modal.querySelector("[name=arrival]");
 
 modal.classList.add("modal-hidden");
 
 button.addEventListener("click", function () {
   if (modal.classList.contains("modal-hidden")) {
     modal.classList.remove("modal-hidden");
-    arrival.focus();
+    modal.classList.add("modal-show");
+     arrival.focus();
   } else {
     modal.classList.add("modal-hidden");
+    modal.classList.remove("modal-show");
   }
 });
