70d506e4a52db90e2353ac6a2220d3b0590d9296
NadiaLev <palash.n@mail.ru>
2020-11-03 13:18:12 +0300

    Внесены финальные правки

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,26 @@ const search = document.querySelector(".search");
 const button = search.querySelector(".button-search");
 const modal = search.querySelector(".modal");
 const arrival = modal.querySelector("[name=arrival]");
+const form = modal.querySelector("form");
+const departure = modal.querySelector("[name=departure]");
 
 modal.classList.add("modal-hidden");
 
-button.addEventListener("click", function () {
-  if (modal.classList.contains("modal-hidden")) {
-    modal.classList.remove("modal-hidden");
-    modal.classList.add("modal-show");
-     arrival.focus();
-  } else {
-    modal.classList.add("modal-hidden");
-    modal.classList.remove("modal-show");
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.toggle("modal-show");
+  modal.classList.toggle("modal-hidden");
+  arrival.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value) {
+    evt.preventDefault();
+    modal.classList.add("modal-error");
+    if (!arrival.value) {
+      arrival.focus();
+    } else {
+      departure.focus();
+     }
   }
 });
